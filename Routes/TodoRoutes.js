35992a5ab58f72bb9cd1f4b8845c51b05758327d
Omaroovee <Omaroovee@gmail.com>
2018-04-12 21:52:51 +0200

    complete update api

--- Routes/TodoRoutes.js
+++ Routes/TodoRoutes.js
@@ -8,7 +8,21 @@ let routes = function (db) {
 
     .post(TodoController.post)
 
-    .put(function (req, res) {})
+    .put(function (req, results) {
+      if (!req.body.id) { results.send('there is nothing selected to update') } else {
+        let id = req.body.id
+        let text = req.body.text
+        let checked = req.body.checked
+        let texts = 'UPDATE todos set text = $1, checked = $2 WHERE id=$3'
+        let VALUES = [text, checked, id]
+        db.query(texts, VALUES, (err, res) => {
+          if (err) { console.log(err.stack) } else {
+            results.send(res.rows[0])
+          }
+        })
+      }
+    })
+
     .delete(function (req, res) {})
 
   return TodoRouter
